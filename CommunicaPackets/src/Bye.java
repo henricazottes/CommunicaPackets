fe48be033b4b32d3d4f88f386680bde7cb31a00a
Henri Cazottes <henri.cazottes@gmail.com>
2015-10-21 11:37:12 +0200

    ajout attr ip classe By

--- CommunicaPackets/src/Bye.java
+++ CommunicaPackets/src/Bye.java
@@ -1,7 +1,18 @@
+import java.net.InetAddress;
+
 
 public class Bye extends Packet{
 	private String nickname;
+	private InetAddress ip;
 	
+	public InetAddress getIp() {
+		return ip;
+	}
+
+	public void setIp(InetAddress ip) {
+		this.ip = ip;
+	}
+
 	public Bye(String nickname){
 		this.nickname = nickname;
 	}
